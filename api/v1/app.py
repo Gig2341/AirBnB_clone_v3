a77a3181e974d8d4c0568abf5d7a01165027a134
michaelmensa <michaelmensa@live.com>
2023-10-01 06:15:44 +0000

    task 5 done

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,8 +4,9 @@ Module: contains the main application
 '''
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from os import getenv
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -15,6 +16,15 @@ app.register_blueprint(app_views)
 def close_db(obj):
     ''' calls close() method '''
     storage.close()
+
+
+@app.errorhandler(404)
+def handle_api_error(exception):
+    '''
+    Returns a JSON-formatted 404 status code respose
+    '''
+    return make_response(jsonify({"error": "Not found"}), 404)
+
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', default='0.0.0.0')
