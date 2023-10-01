28deebe0afbe1f6173f4b08464dfbeab26489d80
michaelmensa <michaelmensa@live.com>
2023-10-01 05:57:10 +0000

    task 4 added some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@ index - index page to run api
 '''
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
 
 @app_views.route('/status', strict_slashes=False)
 def status():
@@ -12,3 +12,18 @@ def status():
     Returns a JSON status
     '''
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    '''
+    Retrieves the number of each object by class
+    '''
+    stats = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+            }
+    return jsonify(stats)
