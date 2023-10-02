1e256910ed12fafb58107a8990c75c88da1482bf
michaelmensa <michaelmensa@live.com>
2023-10-02 00:49:25 +0000

    update api/v1/app.py add CORS instance

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,11 +5,14 @@ Module: contains the main application
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from os import getenv
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+cors = CORS(app, resources={r"/*": {"origins": '0.0.0.0'}})
 
 
 @app.teardown_appcontext
